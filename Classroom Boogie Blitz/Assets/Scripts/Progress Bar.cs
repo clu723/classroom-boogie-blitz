efee1cb4d4ff9698284cac3a22610fd794e09b2e
Runjeet Narula <runjeetnarula@berkeley.edu>
2024-04-23 15:22:18 -0700

    added progress bar but don't know how to make it increase when hands are moving

--- Classroom Boogie Blitz/Assets/Scripts/Progress Bar.cs
+++ Classroom Boogie Blitz/Assets/Scripts/Progress Bar.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     private Slider slider;
-    public float FillSpeed = 0.5f;
+    public float FillSpeed = 0.15f;
     private float targetProgress = 0;
-
+    private LeftController leftController;
+    private RightController rightController;
     // Start is called before the first frame update
     void Start()
     {
         slider = gameObject.GetComponent<Slider>();
+        leftController = gameObject.gameObject.GetComponent<LeftController>();
+        rightController = gameObject.gameObject.GetComponent<RightController>();        
         slider.value = 0;
         IncrementProgress(0.75f);
     }
