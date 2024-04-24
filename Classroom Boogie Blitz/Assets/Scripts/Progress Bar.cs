fc4268e883ce3c261e2fdbd911419e39d427a4e1
Runjeet Narula <runjeetnarula@berkeley.edu>
2024-04-23 17:33:07 -0700

    fixed compile errors

--- Classroom Boogie Blitz/Assets/Scripts/Progress Bar.cs
+++ Classroom Boogie Blitz/Assets/Scripts/Progress Bar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
@@ -7,15 +8,16 @@ public class ProgressBar : MonoBehaviour
     private Slider slider;
     public float FillSpeed = 0.15f;
     private float targetProgress = 0;
-    private LeftController leftController;
-    private RightController rightController;
+    private float leftVelocity = 0;
+    private GameObject leftController;
+
     // Start is called before the first frame update
     void Start()
     {
         slider = gameObject.GetComponent<Slider>();
-        leftController = gameObject.gameObject.GetComponent<LeftController>();
-        rightController = gameObject.gameObject.GetComponent<RightController>();        
+               
         slider.value = 0;
+        leftController = GameObject.Find("LeftController");
         IncrementProgress(0.75f);
     }
 
@@ -25,6 +27,19 @@ public class ProgressBar : MonoBehaviour
         if(slider.value < targetProgress){
             slider.value += FillSpeed * Time.deltaTime;
         }
+
+        //this is the velocity
+        /*
+        get position on this frame.
+        Add it to list
+        average the list with a velocity function 
+        
+        ballSpeed = transform.position - lastPos;
+        ballSpeed /= Time.deltaTime;
+        lastPos = transform.position;
+        */
+
+
     }
 
     public void IncrementProgress(float newProgress)
