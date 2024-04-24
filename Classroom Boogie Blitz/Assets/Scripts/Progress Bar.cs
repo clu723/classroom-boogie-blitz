c063019f4aa8b0afa70ef529f17ab60eb41a95e3
Runjeet Narula <runjeetnarula@berkeley.edu>
2024-04-23 17:40:58 -0700

    handtracking should be working now

--- Classroom Boogie Blitz/Assets/Scripts/Progress Bar.cs
+++ Classroom Boogie Blitz/Assets/Scripts/Progress Bar.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR;
+
 public class ProgressBar : MonoBehaviour
 {
     private Slider slider;
@@ -10,10 +14,18 @@ public class ProgressBar : MonoBehaviour
     private float targetProgress = 0;
     private float leftVelocity = 0;
     private GameObject leftController;
+    private InputData _inputData;
+
+    public TextMeshProUGUI leftScoreDisplay;
+    public TextMeshProUGUI rightScoreDisplay;
+    private float _leftMaxScore = 0f;
+    private float _rightMaxScore = 0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        _inputData = GetComponent<InputData>();
         slider = gameObject.GetComponent<Slider>();
                
         slider.value = 0;
@@ -28,6 +40,20 @@ public class ProgressBar : MonoBehaviour
             slider.value += FillSpeed * Time.deltaTime;
         }
 
+        if (_inputData._leftController.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 leftVelocity))
+        {
+            _leftMaxScore = Mathf.Max(leftVelocity.magnitude, _leftMaxScore);
+            leftScoreDisplay.text = _leftMaxScore.ToString("F2");
+        }
+        if (_inputData._rightController.TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 rightVelocity))
+        {
+            _rightMaxScore = Mathf.Max(rightVelocity.magnitude, _rightMaxScore);
+            rightScoreDisplay.text = _rightMaxScore.ToString("F2");
+        }
+        float average_velocity = (_leftMaxScore + _rightMaxScore) / 2;
+        IncrementProgress(average_velocity);
+        
+        
         //this is the velocity
         /*
         get position on this frame.
